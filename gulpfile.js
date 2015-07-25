397ea17d255dc92aad901ec8dfb1da36d01e8dd3
Anton Popov <a@jokerinteractive.ru>
2015-07-25 14:27:34 +0300

    refactor gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,43 @@
-var gulp        = require('gulp');
-var less        = require('gulp-less');
-var browserSync = require('browser-sync').create();
-var reload      = browserSync.reload;
-var autoprefixer = require('gulp-autoprefixer');
-var uglify = require('gulp-uglify');
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create(),
+    reload = browserSync.reload,
+    uglify = require('gulp-uglify');
 
 gulp.task('html', function() {
   gulp.src("./src/*.html")
-  .pipe(gulp.dest("./dest/"))
+    .pipe(gulp.dest("./dest/"))
 });
 
 gulp.task('image', function() {
   gulp.src("./src/img/*")
-  .pipe(gulp.dest("./dest/img"))
+    .pipe(gulp.dest("./dest/img"))
 });
 
-gulp.task('js-min', function() {
-    gulp.src('./src/js/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./dest/js'))
-        .pipe(reload({stream: true}));
+gulp.task('js', function() {
+  gulp.src('./src/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dest/js'))
+    .pipe(reload({stream: true}));
 });
 
 gulp.task('less', function() {
-  return gulp.src("./src/less/style.less")
+  gulp.src("./src/less/style.less")
     .pipe(less())
     .pipe(autoprefixer())
     .pipe(gulp.dest("./dest/css"))
     .pipe(reload({stream: true}));
 });
 
-gulp.task('serve', ['html', 'less', 'js-min'], function() {
+gulp.task('serve', ['html', 'less', 'js'], function() {
 
   browserSync.init({
     server: './dest/'
   });
   
   gulp.watch("./src/less/**/*.less", ['less']);
-  gulp.watch(".src/js/**/*.js", ['js-min']);
+  gulp.watch("./src/js/**/*.js", ['js']);
   gulp.watch("./src/*.html", ['html']).on('change', reload);
 });
 
